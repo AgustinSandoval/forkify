3485f216a6477c004aa692493a092ecd67c463cd
Agustin Sandoval <agustinsandoval7321@gmail.com>
2022-09-01 19:18:13 -0300

    implementing pagination

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,75 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentEl = document.querySelector('.pagination');
+  _addHandlerClick(handler) {
+    this._parentEl.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
+    //page 1, and there are other pages
+    if (currentPage === 1 && numPages > 1) {
+      return `
+        ${this._buttonPagination('next')}
+      `;
+    }
+    //last page
+    if (currentPage === numPages && numPages > 1) {
+      return `
+        ${this._buttonPagination('prev')}
+      `;
+    }
+    //middle page
+    if (currentPage < numPages) {
+      return `
+        ${this._buttonPagination('prev')}
+        ${this._buttonPagination('next')}
+      `;
+    }
+    //page 1, and there are NO other pages
+    return '';
+  }
+  _buttonPagination(direction) {
+    const currentPage = this._data.page;
+
+    if (direction === 'next') {
+      return `
+        <button data-goto="${
+          currentPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+        `;
+    }
+    if (direction === 'prev') {
+      return `
+        <button data-goto="${
+          currentPage - 1
+        }" class="btn--inline pagination__btn--prev">  
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${currentPage - 1}</span>
+        </button>
+        `;
+    }
+  }
+}
+
+export default new PaginationView();
